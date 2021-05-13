640215cd33ab357fe3a0e5cee9e3ffa5ae91f70b
Taufan <taufan@airasiaredtix.com>
2021-05-13 10:08:50 +0700

    Ignore env

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,6 +1,6 @@
 import { baseUrl } from "@env";
 
-const isOnline = false;
-const url = isOnline ? "baseUrl" : "http://192.168.1.111:4000/api/v1";
+const isOnline = true;
+const url = isOnline ? `${baseUrl}/api/v1` : "http://192.168.1.111:4000/api/v1";
 
 export default url;
