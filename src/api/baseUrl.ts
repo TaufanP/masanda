95cc1018a37444f654e3fd423df644da2c033286
Taufan <taufan@airasiaredtix.com>
2021-05-16 15:48:40 +0700

    Change base url

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,6 +1,6 @@
 import { baseUrl } from "@env";
 
-const isOnline = false;
+const isOnline = true;
 const url = isOnline ? `${baseUrl}/api/v1` : "http://192.168.1.111:4000/api/v1";
 
 export default url;
